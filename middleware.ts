b1292505d31d1c2fc3e1b3a75723af3b72020155
icloudjonalves-cmd <jonathan.alves@emserh.ma.gov.br>
2025-10-29 20:10:13 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  '/',
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isPublicRoute(req)) return;
-  auth().protect();
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: [
-    '/((?!.+\\.[\\w]+$|_next).*)',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api)(.*)"],
 };
