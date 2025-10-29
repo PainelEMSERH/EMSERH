689fb06662f878e3edaa0650bf37585f6d6d7611
icloudjonalves-cmd <jonathan.alves@emserh.ma.gov.br>
2025-10-29 15:32:49 -0300

    ...

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
-import { authMiddleware } from '@clerk/nextjs'
-export default authMiddleware({
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
+export default clerkMiddleware({
   publicRoutes: ['/sign-in(.*)', '/sign-up(.*)', '/favicon.ico', '/images/(.*)']
 })
-export const config = { matcher: ['/((?!_next|.*\..*).*)'] }
+
+export const config = {
+  matcher: ['/((?!_next|.*\..*).*)'],
+}
